3cae4eed3ded4022142f629c9c6d0271135dec80
Dustin Martens <dmartens.it@gmail.com>
2020-03-30 14:43:40 -0500

    Added appbar to the project

--- app/src/main/java/ca/dragz/chequemate/MainActivity.java
+++ app/src/main/java/ca/dragz/chequemate/MainActivity.java
@@ -1,14 +1,55 @@
 package ca.dragz.chequemate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    //to inflate the xml menu file
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.appbar, menu);
+        return true;
+    }
+
+    //to handle events
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        boolean returnVal = false;
+
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                Log.d("DGM", "refresh menu item");
+                returnVal = true;
+                break;
+            case R.id.action_settings:
+                Log.d("DGM", "settings menu item");
+//                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+//                startActivityForResult(i, STANDARD_REQUEST_CODE);
+                returnVal = true;
+                break;
+        }
+        return returnVal;
     }
 }
