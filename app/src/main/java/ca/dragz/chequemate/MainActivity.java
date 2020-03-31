eaf7cd805eaa0e1849191d8390ff490bed593c31
Dustin Martens <dmartens.it@gmail.com>
2020-03-30 19:26:10 -0500

    Added DatePicker and TimePicker dialogs to retrieve the user selected date and time for a shift

--- app/src/main/java/ca/dragz/chequemate/MainActivity.java
+++ app/src/main/java/ca/dragz/chequemate/MainActivity.java
@@ -4,7 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,33 +13,50 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-    Button button;
+
+    Button btnSetDate;
+    Button btnStartTime;
+    Button btnEndTime;
+
+    TextView tvDate;
+    TextView tvStartTime;
+    TextView tvEndTime;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("message");
+        EventHandler eventHandler = new EventHandler();
 
-                myRef.setValue("Hello, World!");
-            }
-        });
+        tvDate = findViewById(R.id.tvDate);
+        tvStartTime = findViewById(R.id.tvStartTime);
+        tvEndTime = findViewById(R.id.tvEndTime);
+
+        btnSetDate = findViewById(R.id.btnSetDate);
+        btnStartTime = findViewById(R.id.btnStartTime);
+        btnEndTime = findViewById(R.id.btnEndTime);
+
+        btnSetDate.setOnClickListener(eventHandler);
+        btnStartTime.setOnClickListener(eventHandler);
+        btnEndTime.setOnClickListener(eventHandler);
     }
 
     //to inflate the xml menu file
@@ -68,5 +86,59 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return returnVal;
+    }
+
+    public class EventHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.btnSetDate:
+//                    mDatabase.child("shift_entries").child("shift").setValue("Hello, Universe!");
+                    ShowDatePicker();
+                    break;
+                case R.id.btnStartTime:
+                    ShowTimePicker(true);
+                    break;
+                case R.id.btnEndTime:
+                    ShowTimePicker(false);
+                    break;
+            }
+        }
+    }
+
+    public void ShowDatePicker() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        tvDate.setText((month + 1) + "/" + day + "/" + year);
+                    }
+                }, year, month, dayOfMonth);
+        datePickerDialog.show();
+    }
+
+    public void ShowTimePicker(final boolean isStartTime) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        if (isStartTime) {
+                            tvStartTime.setText(hourOfDay + ":" + minute);
+                        } else {
+                            tvEndTime.setText(hourOfDay + ":" + minute);
+                        }
+
+                    }
+                }, hour, minute, false);
+        timePickerDialog.show();
     }
 }
